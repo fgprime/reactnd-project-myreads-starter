5285631798adbfadc01b02c84c84b3cd4b4fbced
Franz Geiselbrechtinger <fg@primeinteractive.de>
2021-07-23 08:15:19 +0200

    feat: Add book search via API and showing results

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import Book from './Book';
 
 class BookSearch extends React.Component {
+    state = {
+        query: '',
+        results: [],
+    };
+
+    updateSearch = (event) => {
+        const query = event.target.value;
+        this.setState((prevState) => ({
+            query: query,
+        }));
+
+        if (query === '') {
+            /*
+             * Search with an empty query string leads to an 403 in the API
+             * Therefore it will be handled manually and results will be emptied
+             */
+            this.setState((prevState) => ({
+                results: [],
+            }));
+        } else {
+            BooksAPI.search(query).then((books) => {
+                if (Array.isArray(books)) {
+                    this.setState((prevState) => ({
+                        results: books,
+                    }));
+                } else {
+                    /*
+                     * Handle empty result from API
+                     */
+                    this.setState((prevState) => ({
+                        results: [],
+                    }));
+                }
+            });
+        }
+    };
+
     render() {
         return (
             <div className="search-books">
@@ -10,22 +49,25 @@ class BookSearch extends React.Component {
                         Close
                     </Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
                         <input
                             type="text"
                             placeholder="Search by title or author"
+                            value={this.state.query}
+                            onChange={this.updateSearch}
                         />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid" />
+                    <ol className="books-grid">
+                        {this.state.results.map((book) => (
+                            <li key={book.id}>
+                                <Book
+                                    book={book}
+                                    updateBookShelf={this.props.updateBookShelf}
+                                />
+                            </li>
+                        ))}
+                    </ol>
                 </div>
             </div>
         );
